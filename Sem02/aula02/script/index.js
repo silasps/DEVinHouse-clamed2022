27400d914ba84a46cbc7834174919e4e8aa00433
silasps <silaspereiras@gmail.com>
2022-07-07 15:44:22 -0300

    Arquivos das aulas

---
+++ Sem02/aula02/script/index.js
@@ -0,0 +1,34 @@
+// var idade = parseInt(window.prompt("Digite sua idade"));
+// document.write(idade);
+
+// Imprimir o valor na tela
+
+// var isConfirm = window.confirm();
+// document.write("<h2>Retorno do confirm</h2>");
+// document.write("<h1>"+isConfirm + "<h1>");
+// document.write(typeof isConfirm);
+
+
+// Imprimir o valor no console da página
+
+// var isConfirm = window.confirm("Amigo, realmente estou aqui?");
+// console.log("Retorno do confirm");
+// console.log(isConfirm);
+// console.log(typeof isConfirm);
+// console.log(12);
+
+
+// Somente alerta, não grava nada
+
+// window.alert(12);
+// window.alert("oi");
+
+
+// Exercício visto em aula = slide 17
+
+var nome = window.prompt("Digite seu nome:");
+var sobrenome = window.prompt("Digite seu sobrenome:");
+console.log(nome + " " + sobrenome);
+//ou
+console.log(nome, sobrenome);
+document.write("Seja bem vindo <h1>" + nome + " " + sobrenome + "</h1>");
---
+++ Sem02/aula03/index.js
@@ -0,0 +1,16 @@
+var nota1 = parseFloat(window.prompt("Digite a primeira nota."));
+var nota2 = parseFloat(window.prompt("Digite a segunda nota."));
+
+
+
+var notaTotal = nota1 + nota2;
+
+document.write("<h1>" + 'Sua nota é ' + notaTotal + '.');
+
+if (notaTotal >= 7 && notaTotal < 10) {
+  document.write("<h2>" + 'Aprovado' + "</h2>");
+} else if (notaTotal < 7) {
+  document.write("<h2>" + 'Reprovado' + "</h2>");
+} else if (notaTotal == 10) {
+    document.write("<h1>" + 'Você arrasou. Aprovado com excelência.' + "</h1>");
+}
