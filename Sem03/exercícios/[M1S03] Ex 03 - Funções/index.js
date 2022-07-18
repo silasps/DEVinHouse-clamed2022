318c24b95115ae4100c1362cfb835f70e9ae3926
silasps <silaspereiras@gmail.com>
2022-07-18 11:55:34 -0300

    stashing

---
+++ Sem03/exercícios/[M1S03] Ex 03 - Funções/index.js
@@ -0,0 +1,24 @@
+// // Codigo do Michael
+
+// var lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var listaTamanho = lista.length;
+// var contadorPar = 0;
+// var contadorImpar = 0;
+// function ContadorParImpar(lista) {
+//   if (lista % 2 == 0) {
+//     contadorPar++;
+//   } else if (!(lista % 2 != 1)) {
+//     contadorImpar++;
+//   }
+// }
+
+// lista.forEach(ContadorParImpar);
+// console.log(
+//   'A quantidade informada foi',
+//   listaTamanho,
+//   'a de valores pares foi ',
+//   contadorPar,
+//   ', e a de valores ímpares foi ',
+//   contadorImpar
+// );
+
