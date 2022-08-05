675e5a0e41edc228d4843e9cca1e085c8f54e17f
silasps <silaspereiras@gmail.com>
2022-08-04 21:34:10 -0300

    "in class"

--- Week06/04.08/index.js
+++ Week06/04.08/index.js
@@ -34,22 +34,3 @@ botao.addEventListener('click', () => {
 
 
 });
-
-
-
-// ===================
-// function(){
-
-// 	const cep = document.querySelector("input[name=cep]");
-  
-//   cep.addEventListener('blur', e=> {
-//   		const value = cep.value.replace(/[^0-9]+/, '');
-//       const url = `https://viacep.com.br/ws/${value}/json/`;
-      
-//       fetch(url)
-//       .then( response => response.json())
-//       .then( json => {
-      		
-//           if( json.logradouro ) {
-//           	document.querySelector('input[name=rua]').value = json.logradouro;
-//             document.querySelector('input[name=bairro]').value = jso
