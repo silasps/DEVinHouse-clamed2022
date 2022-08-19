85e348bc3d009f24b94d226d586309437d7de9dc
silasps <silaspereiras@gmail.com>
2022-08-19 10:38:06 -0300

    "aula-04"

---
+++ Week07/aula-04/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RenderizacaoCondicional from './components/RendCondic';
+
+function App() {
+  return (
+    <div className="App">
+      <RenderizacaoCondicional isLoading={false}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Week07/aula-04/src/components/RendCondic.js
@@ -0,0 +1,13 @@
+export default function RenderizacaoCondicional({isLoading}) {
+    
+    if (isLoading) {
+        return <p>Carregando...</p>
+    }
+
+    return (
+        <>
+            <h1>Aula 04</h1>
+            <p>A aplicacao carregou!</p>
+        </>
+    )
+}
