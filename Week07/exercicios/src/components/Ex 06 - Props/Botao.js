1b3b505e180369caa82262ed5df5868edf489f69
silasps <silaspereiras@gmail.com>
2022-08-20 12:01:59 -0300

    'Ex 06.'

--- Week07/exercicios/src/components/Ex 06 - Props/Botao.js
+++ Week07/exercicios/src/components/Ex 06 - Props/Botao.js
@@ -1,3 +1,5 @@
+// Crie um componente de botão com as props background (Cor de fundo do botão) e title (Texto do botão).
+
 export default function Botao({ title, background, color }) {
     return <h1 style={{ backgroundColor: background, color: color }}>{title}</h1>
 }
