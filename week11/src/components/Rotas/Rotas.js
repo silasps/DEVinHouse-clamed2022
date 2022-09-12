093fb27cf1c5602f9dc9ff81ff8023f94687a551
silasps <silaspereiras@gmail.com>
2022-09-12 13:19:02 -0300

    Ex 01

--- week11/src/components/Rotas/Rotas.js
+++ week11/src/components/Rotas/Rotas.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes, Navigate  } from 'react-router-dom'
+import PaginaWeb from '../../exercícios/Ex 01 - Hd/PaginaWeb';
 import Home from '../Formularios/Home';
 import Login from '../Formularios/Login';
 import NotFound from '../Formularios/NotFound';
@@ -16,7 +17,7 @@ export default function Rotas() {
           <Route path="/login" element={<Login />} />
           <Route path="/entrar" element={<Navigate replace to="/login" />} />
 
-          <Route path='/home' element={<Home />}/>
+          <Route path='/home' element={<PaginaWeb />}/>
           <Route path='/' element={<Navigate replace to='/home'/>} />
   
           <Route path="*" element={<NotFound />} />
---
+++ week11/src/exercícios/Ex 01 - Hd/PaginaWeb.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function PaginaWeb() {
+  return (
+    <div>
+      <Header />
+      <h1>Conteúdo da Página</h1>
+      <Footer />
+    </div>
+  );
+}
